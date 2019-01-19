34dbacd8285fb19b6d3807670bf8b508291da6d6
Yakov <yakov_dux@bk.ru>
2019-01-19 17:40:41 +0400

    Header is ready

--- app/js/common.js
+++ app/js/common.js
@@ -1,14 +1,14 @@
 $(function() {
 
 	$('#my-menu').mmenu({
-		extensions: ['widescreen', 'theme-black', 'effect-menu-slide', 'pagedim-black', 'position-right'],
+		extensions: ['theme-black', 'effect-menu-slide', 'pagedim-black', 'position-right'],
 		navbar: {
 			title: '<img src="img/logo-1.svg" alt="Салоно красоты СМитлер">'
 		}
 	});
 
     var api = $("#my-menu").data( "mmenu" );
-    api.bind('open:start', function () {
+    api.bind('open:finish', function () {
         $('.hamburger').toggleClass('is-active');
     }).bind('close:finish', function() {
         $('.hamburger').removeClass('is-active');
