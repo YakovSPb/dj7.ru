0bef7f862206e540c90a1eaaf787c1cc2732f2b8
Yakov <yakov_dux@bk.ru>
2019-01-22 19:30:52 +0400

    Footer is ready

--- app/js/common.js
+++ app/js/common.js
@@ -16,7 +16,45 @@ $(function () {
         autoHeight: true
     });
 
+    $('.partners').owlCarousel({
+        loop: true,
+        smartSpeed: 700,
+        dots: false,
+        nav: true,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            },
+            1200: {
+                items: 4
+            }
+        }
+    });
+
     $(document).ready(function() {
+
+
+        $(window).scroll(function(){
+           if ($(this).scrollTop() > $(this).height()){
+            $('.top').addClass('active');
+           } else {
+               $('.top').removeClass('active');
+           }
+        });
+
+        $('.top').click(function(){
+            $('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
+        });
+
+
 
         //E-mail Ajax Send
         $("form.callback").submit(function() { //Change
@@ -71,6 +109,7 @@ $(function () {
 
 
 
+
     function carouselService() {
         $('.carousel-services-item').each(function () {
             var ths = $(this),
@@ -99,3 +138,7 @@ function onResize(){
 window.onresize = function() {onResize()};
 
 });
+
+$(window).on('load', function(){
+    $('.preloader').delay(1000).fadeOut('slow');
+})
